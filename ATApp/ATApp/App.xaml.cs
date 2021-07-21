c13f29d2acbd80dc184694390b84064a69121fbc
Leandro Vasquez <leandro.vasquez.mora@gmail.com>
2021-07-21 15:12:34 -0400

    PRIMER COMMIT

---
+++ ATApp/ATApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ATApp.Services;
+using ATApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ATApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
